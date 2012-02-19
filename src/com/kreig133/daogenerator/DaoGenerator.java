dd44b3b1b73f8eb22e20e9ab600d4768d3df04d3
phpusr <phpusr@gmail.com>
2012-02-19 22:28:16 +0600

    Начал реализацию сохранения параметров в файл настроек

--- src/com/kreig133/daogenerator/DaoGenerator.java
+++ src/com/kreig133/daogenerator/DaoGenerator.java
@@ -1,16 +1,14 @@
 package com.kreig133.daogenerator;
 
-import com.kreig133.daogenerator.common.settings.FunctionSettings;
-import com.kreig133.daogenerator.common.settings.FunctionSettingsImpl;
 import com.kreig133.daogenerator.common.settings.OperationSettings;
 import com.kreig133.daogenerator.common.settings.OperationSettingsImpl;
-import com.kreig133.daogenerator.files.mybatis.MyBatis;
 import com.kreig133.daogenerator.gui.MainForm;
-import com.sun.corba.se.spi.orb.Operation;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
+import java.net.URISyntaxException;
+import java.util.Properties;
 
 /**
  * @author eshangareev
@@ -30,10 +28,30 @@ public class DaoGenerator {
                 JFrame frame = new JFrame( "MainForm" );
                 frame.setContentPane          ( new MainForm().panel1 );
                 frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-                frame.pack();
+                frame.setSize(500, 300);
+                //frame.set //Установить точку где будет показано окно
+                //frame.pack();
                 frame.setVisible( true );
+
+                loadSettings();
             }
         } );
+    }
+
+    private static void loadSettings() {
+        Properties settings=new Properties();
+        try{
+            File file = new File(DaoGenerator.class.getResource("DaoGenerator.ini").toURI());
+            FileInputStream in=new FileInputStream(file);
+            settings.load(in);
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        int h=Integer.parseInt(settings.getProperty("height","200"));
+        int w=Integer.parseInt(settings.getProperty("width","300"));
+        //setSize(w,h);
     }
 
     public static OperationSettings getCurrentOperationSettings(){
