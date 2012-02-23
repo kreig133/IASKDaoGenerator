9d86c7cfe3578921f43510061f62598238ee5e57
phpusr <phpusr@gmail.com>
2012-02-23 12:42:56 +0500

    Перенес имена настроек в другой пакет

--- src/com/kreig133/daogenerator/DaoGenerator.java
+++ src/com/kreig133/daogenerator/DaoGenerator.java
@@ -3,6 +3,7 @@ package com.kreig133.daogenerator;
 import com.kreig133.daogenerator.common.settings.OperationSettings;
 import com.kreig133.daogenerator.common.settings.OperationSettingsImpl;
 import com.kreig133.daogenerator.gui.MainForm;
+import org.dyndns.phpusr.daogenerator.settings.SettingName;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,6 +20,10 @@ public class DaoGenerator {
 
     private static OperationSettings operationSettings;
     public static final String DAO_GENERATOR_PROPERTIES = "DaoGenerator.properties";
+    public static final String MAPPING_PACKAGE = "mappingPackage";
+    public static final String IASK = "iask";
+    public static final String DEPO = "depo";
+    public static final String SKIP_TESTS = "skipTests";
 
 
     public static void main(String[] args) throws IOException {
@@ -30,12 +35,10 @@ public class DaoGenerator {
                 JFrame frame = new JFrame( "MainForm" );
                 frame.setContentPane          ( new MainForm().panel1 );
                 frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-                //frame.setSize(500, 300);
                 //frame.set //Установить точку где будет показано окно
-                //frame.pack();
-                //frame.setVisible( true );
 
                 loadSettings(frame);
+                frame.setVisible(true);
                 //System.exit(0);
             }
         } );
@@ -50,16 +53,18 @@ public class DaoGenerator {
         } catch(IOException e){
             e.printStackTrace();
         }
-        final int w=Integer.parseInt(settings.getProperty("width","500"));
-        final int h=Integer.parseInt(settings.getProperty("height","200"));
-        final String sourceDir = settings.getProperty("sourceDir", "D:\\");
-        final String destDir = settings.getProperty("destDir", "D:\\");
-        final String entityPackage = settings.getProperty("entityPackage", "ru.sbrf.iask.so.aplana.persistence.entity.op148");
-        final String interfacePackage = settings.getProperty("interfacePackage", "ru.sbrf.iask.so.aplana.persistence.dao");
-        final String mappingPackage = settings.getProperty("mappingPackage", "ru.sbrf.iask.so.aplana.persistence.data");
-        final boolean iask = Boolean.parseBoolean(settings.getProperty("iask", "1"));
-        final boolean depo = Boolean.parseBoolean(settings.getProperty("depo", "0"));
-        final boolean skipTests = Boolean.parseBoolean(settings.getProperty("skipTests","0"));
+        final int width=Integer.parseInt(settings.getProperty(SettingName.WIDTH,"500"));
+        final int height=Integer.parseInt(settings.getProperty(SettingName.HEIGHT,"200"));
+        final String sourceDir = settings.getProperty(SettingName.SOURCE_DIR, "D:\\");
+        final String destDir = settings.getProperty(SettingName.DEST_DIR, "D:\\");
+        final String entityPackage = settings.getProperty(SettingName.ENTITY_PACKAGE, "ru.sbrf.iask.so.aplana.persistence.entity.op148");
+        final String interfacePackage = settings.getProperty(SettingName.INTERFACE_PACKAGE, "ru.sbrf.iask.so.aplana.persistence.dao");
+        final String mappingPackage = settings.getProperty(MAPPING_PACKAGE, "ru.sbrf.iask.so.aplana.persistence.data");
+        final boolean iask = Boolean.parseBoolean(settings.getProperty(IASK, "1"));
+        final boolean depo = Boolean.parseBoolean(settings.getProperty(DEPO, "0"));
+        final boolean skipTests = Boolean.parseBoolean(settings.getProperty(SKIP_TESTS,"0"));
+
+        //TODO сделать загрузку с помощью properties.load( props );
 
         final Component[] components = frame.getContentPane().getComponents();
         ((JTextField)components[0]).setText(sourceDir);
@@ -71,8 +76,7 @@ public class DaoGenerator {
         ((JRadioButton)components[13]).setSelected(iask);
         ((JRadioButton)components[14]).setSelected(depo);
 
-        frame.setSize(w, h);
-        frame.setVisible(true);
+        frame.setSize(width, height);
     }
 
     public static OperationSettings getCurrentOperationSettings(){
