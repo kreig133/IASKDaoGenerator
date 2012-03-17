838fae9c9878270209046fda6ccda737876f2466
kreig133 <kreig133@gmail.com>
2012-03-18 00:33:09 +0500

    Внешний вид программы ( режим аналитика ). По-новому расскидал элементы.

--- src/main/java/com/kreig133/daogenerator/gui/FirstForm.java
+++ src/main/java/com/kreig133/daogenerator/gui/FirstForm.java
@@ -14,6 +14,8 @@ import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Enumeration;
