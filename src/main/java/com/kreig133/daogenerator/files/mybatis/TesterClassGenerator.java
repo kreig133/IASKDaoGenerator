3f0b85f7aea1eab0c3e0c9f3d164a1813a7925c7
eshangareev <eshangareev@aplana.com>
2012-03-23 13:08:25 +0600

    Генератор JUnit-теста

--- src/main/java/com/kreig133/daogenerator/files/mybatis/TesterClassGenerator.java
+++ src/main/java/com/kreig133/daogenerator/files/mybatis/TesterClassGenerator.java
@@ -7,6 +7,7 @@ import com.kreig133.daogenerator.enums.Scope;
 import com.kreig133.daogenerator.files.JavaClassGenerator;
 import com.kreig133.daogenerator.files.mybatis.mapping.MappingGenerator;
 import com.kreig133.daogenerator.jaxb.DaoMethod;
+import com.kreig133.daogenerator.jaxb.ParameterType;
 import com.kreig133.daogenerator.settings.Settings;
 
 import java.io.File;
@@ -22,7 +23,9 @@ public class TesterClassGenerator extends JavaClassGenerator{
     @Override
     public File getFile() throws IOException {
         final File file = new File(
-                Settings.settings().getPathForGeneratedTests() + "/" + getFileName() + JAVA_EXTENSION
+                Settings.settings().getPathForGeneratedTests() + "/"
+                        + replacePointBySlash( Settings.settings().getMapperPackage() )+ "/" + getFileName()
+                        + JAVA_EXTENSION
         );
         createDirsAndFile( file.getParentFile() );
         return file;
@@ -32,27 +35,27 @@ public class TesterClassGenerator extends JavaClassGenerator{
     public void generateHead() throws IOException {
         insertPackageLine( Settings.settings().getMapperPackage() );
         insertLine();
+        insertImport( "com.aplana.sbrf.AbstractDepoDaoExecuteTester" );
         insertImport( "org.junit.Test" );
         insertImport( "org.junit.runner.RunWith" );
         insertImport( "org.springframework.beans.factory.annotation.Autowired" );
         insertImport( "org.springframework.test.context.ContextConfiguration" );
         insertImport( "org.springframework.test.context.junit4.SpringJUnit4ClassRunner" );
+        insertImport( Settings.settings().getMapperPackage() + "." + MappingGenerator.instance().getFileName() );
         insertLine();
-        insertImport( "java.text.SimpleDateFormat" );
+        insertImport( "java.util.HashMap" );
+        insertImport( "java.util.Map" );
         insertLine();
         builder.append( "@RunWith(SpringJUnit4ClassRunner.class)" );
         insertLine();
         builder.append( "@ContextConfiguration(locations = \"").append( MavenProjectGenerator.getConfigName() )
                 .append( ".xml\" )" );
         insertLine();
-        insertClassDeclaration( ClassType.Class, getFileName(), null, null );
-        insertTabs( 1 ).append( "SimpleDateFormat format = new SimpleDateFormat( \"M-d-yyyy H:m:s.SSS\" );" );
-        insertLine();
-        insertLine();
+        insertClassDeclaration( ClassType.Class, getFileName(), "AbstractDepoDaoExecuteTester", null );
         insertTabs( 1 ).append( "@Autowired" );
         insertLine();
         insertTabs( 1 ).append( MappingGenerator.instance().getFileName() ).append( " " )
-                .append( convertNameForNonClassNaming( MappingGenerator.instance().getFileName() ) ).append( ";" );
+                .append( "dao" ).append( ";" );
         insertLine();
     }
 
@@ -66,11 +69,28 @@ public class TesterClassGenerator extends JavaClassGenerator{
                 null,
                 daoMethod.getCommon().getMethodName() + "RunTest",
                 null,
-                Arrays.asList( "ParseException" ),
+                null,
                 false
         );
-
-
+        insertLine();
+        insertTabs( 2 ).append( "final Map<String, String> values = new HashMap<String, String>();" );
+        insertLine();
+        for ( ParameterType parameterType : daoMethod.getInputParametrs().getParameter() ) {
+            insertTabs( 2 ).append( "values.put( \"" ).append( parameterType.getRenameTo() ).append( "\", \"" )
+                    .append( parameterType.getTestValue() ).append( "\" );" );
+            insertLine();
+        }
+        insertLine();
+        insertTabs( 2 ).append( "final String methodName = \"" ).append( daoMethod.getCommon().getMethodName() )
+                .append( "\";" );
+        insertLine();
+        insertLine();
+        insertTabs( 2 ).append( "Object invoke = invoke( dao, values, methodName );" );
+        insertLine();
+        insertLine();
+        insertTabs( 2 ).append( "org.junit.Assert.assertNotNull( invoke );" );
+        insertLine();
+        insertLine();
         insertTabs( 1 ).append( "}" );
         insertLine();
     }
