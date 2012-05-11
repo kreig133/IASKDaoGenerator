a054f40f1b9ca3dddd020349b9c53c0d797261d3
eshangareev <eshangareev@aplana.com>
2012-05-11 10:48:01 +0600

    Рефакторинг билдеров. Выкинул лишние классы.

--- src/main/java/com/kreig133/daogenerator/files/builder/MapperFileBuilder.java
+++ src/main/java/com/kreig133/daogenerator/files/builder/MapperFileBuilder.java
@@ -1,15 +1,36 @@
 package com.kreig133.daogenerator.files.builder;
 
-import com.kreig133.daogenerator.settings.Settings;
+import com.kreig133.daogenerator.files.JavaClassGenerator;
+import com.kreig133.daogenerator.files.mybatis.mapping.MappingGenerator;
+import com.kreig133.daogenerator.files.mybatis.test.TesterClassGenerator;
+import com.kreig133.daogenerator.jaxb.DaoMethod;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * @author eshangareev
  * @version 1.0
  */
-abstract public class MapperFileBuilder extends OneClassForOperationFileBuilder {
+public class MapperFileBuilder extends FileBuilder {
+
+    @Override
+    public void generateBody( @NotNull List<DaoMethod> daoMethods ) {
+        for ( DaoMethod daoMethod : daoMethods ) {
+            for ( JavaClassGenerator generator : generators ) {
+                generator.generateBody( daoMethod );
+            }
+        }
+    }
+
     @NotNull
     public static FileBuilder newInstance() {
-        return new DepoMapperFileBuilder();
+        return new MapperFileBuilder();
+    }
+
+    @Override
+    protected void prepareBuilder( List<DaoMethod> daoMethod ) {
+        generators.add( MappingGenerator.instance() );
+        generators.add( TesterClassGenerator.instance() );
     }
 }
--- src/main/java/com/kreig133/daogenerator/files/builder/ParameterClassBuilder.java
+++ src/main/java/com/kreig133/daogenerator/files/builder/ParameterClassBuilder.java
@@ -1,16 +1,39 @@
 package com.kreig133.daogenerator.files.builder;
 
-import com.kreig133.daogenerator.settings.Settings;
+import com.kreig133.daogenerator.files.JavaClassGenerator;
+import com.kreig133.daogenerator.files.mybatis.DaoJavaClassGenerator;
+import com.kreig133.daogenerator.files.mybatis.model.InModelClassGenerator;
+import com.kreig133.daogenerator.files.mybatis.model.OutModelClassGenerator;
+import com.kreig133.daogenerator.jaxb.DaoMethod;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * @author eshangareev
  * @version 1.0
  */
-abstract public class ParameterClassBuilder extends OneClassForEachMethodFileBuilder {
+class ParameterClassBuilder extends FileBuilder {
+
+    @Override
+    public void generateBody( List<DaoMethod> daoMethods ) {
+        for ( JavaClassGenerator generator : generators ) {
+            generator.generateBody( new DaoMethod() );
+        }
+    }
 
     @NotNull
     public static FileBuilder newInstance() {
-        return new DepoParameterClassBuilder();
+        return new ParameterClassBuilder();
+    }
+
+    @Override
+    protected void prepareBuilder( @NotNull List<DaoMethod> daoMethod ) {
+        for ( DaoMethod method : daoMethod ) {
+            if( DaoJavaClassGenerator.checkToNeedOwnInClass( method ) )
+                generators.add( InModelClassGenerator .newInstance( method ) );
+            if( DaoJavaClassGenerator.checkToNeedOwnOutClass( method ) )
+                generators.add( OutModelClassGenerator.newInstance( method ) );
+        }
     }
 }
