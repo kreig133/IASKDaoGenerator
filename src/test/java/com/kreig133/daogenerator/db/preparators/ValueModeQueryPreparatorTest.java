d3e712172d58b8d3144a3ab0af605d85a0ac09a3
eshangareev <eshangareev@aplana.com>
2012-05-12 17:09:56 +0600

    Подправил регэксп для считывания в ValueMode(бага с тем, что игнорировались эскейп последовательности )

--- src/test/java/com/kreig133/daogenerator/db/preparators/ValueModeQueryPreparatorTest.java
+++ src/test/java/com/kreig133/daogenerator/db/preparators/ValueModeQueryPreparatorTest.java
@@ -1,6 +1,7 @@
 package com.kreig133.daogenerator.db.preparators;
 
 import junit.framework.Assert;
+import org.intellij.lang.annotations.Language;
 import org.junit.Test;
 
 import java.util.Map;
@@ -10,12 +11,28 @@ import java.util.Map;
  * @version 1.0
  */
 public class ValueModeQueryPreparatorTest extends ValueModeQueryPreparator {
+
+    private static final String QUERY = "SELECT\n" +
+            "\ttDocPack.nDocID\n" +
+            "FROM \n" +
+            "\ttDocPack ( NOLOCK )\n" +
+            "WHERE \n" +
+            "\t``tDocPack.nDocTypeID = 59 and\n" +
+            "\t``isnull ( tDocPack.ndeleted , 0 ) = 0 and\n" +
+            "\ttDocPack.nPackID = 250287 ";
+
     @Test
     public void columnsFromQueryTest() {
         Map<String, String> columnsFromQuery = super.getColumnsFromQuery( QueryPreparatorTest.updateQuery );
         Assert.assertEquals( columnsFromQuery.size(), 2 );
         Assert.assertTrue( columnsFromQuery.keySet().contains( "nflreport" ) );
         Assert.assertTrue( columnsFromQuery.keySet().contains( "n_DEPO_id" ) );
+    }
+
+    @Test
+    public void columnsFromQueryTestWithEscapedColumns(){
+        Map<String, String> columnsFromQuery = super.getColumnsFromQuery( QUERY );
+        Assert.assertEquals( columnsFromQuery.size(), 1 );
     }
 
 }
