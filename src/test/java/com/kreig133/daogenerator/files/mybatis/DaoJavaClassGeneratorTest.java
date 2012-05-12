df8e0e84fe3730e48e559e8bbf725c1505c432ca
eshangareev <eshangareev@aplana.com>
2012-05-12 14:00:27 +0600

    Добавил обработку имен ( если все буквы заглавные, то конвертим их в строчные ). + Раскидал тесты.

---
+++ src/test/java/com/kreig133/daogenerator/files/mybatis/DaoJavaClassGeneratorTest.java
@@ -0,0 +1,34 @@
+package com.kreig133.daogenerator.files.mybatis;
+
+import com.kreig133.daogenerator.jaxb.DaoMethod;
+import com.kreig133.daogenerator.jaxb.ParameterType;
+import com.kreig133.daogenerator.jaxb.ParametersType;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static com.kreig133.daogenerator.files.mybatis.DaoJavaClassGenerator.checkToNeedOwnInClass;
+
+/**
+ * @author eshangareev
+ * @version 1.0
+ */
+public class DaoJavaClassGeneratorTest {
+    @Test
+    public void testCheckToNeedOwnInClass(){
+
+        final DaoMethod daoMethod = new DaoMethod();
+        daoMethod.setInputParametrs( new ParametersType() );
+        daoMethod.getInputParametrs().getParameter().add( new ParameterType() );
+
+        Assert.assertFalse( checkToNeedOwnInClass( daoMethod ) );
+
+        daoMethod.getInputParametrs().getParameter().add( new ParameterType() );
+
+        Assert.assertFalse( checkToNeedOwnInClass( daoMethod ) );
+
+        daoMethod.getInputParametrs().getParameter().add( new ParameterType() );
+        daoMethod.getInputParametrs().getParameter().add( new ParameterType() );
+
+        Assert.assertTrue( checkToNeedOwnInClass( daoMethod ) );
+    }
+}
