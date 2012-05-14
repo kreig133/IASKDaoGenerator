2f540c154eff65898b3275c17a64650cd6f2fce1
eshangareev <eshangareev@aplana.com>
2012-05-14 11:18:45 +0600

    Переделал тесты под базу ИАСКа.

--- src/main/java/com/kreig133/daogenerator/db/preparators/QueryPreparator.java
+++ src/main/java/com/kreig133/daogenerator/db/preparators/QueryPreparator.java
@@ -38,25 +38,26 @@ public class QueryPreparator {
     @Language( "RegExp" )
     protected static final String TEST_VALUES = "(([-\\d\\.]+)|(null)|('.+?'))";
     @NotNull
-    @Language("RegExp")
+    @Language( "RegExp" )
     protected String regex = "(?u)[^\\.`]([\"\\[]?\\b\\w+[\"\\]]?\\.){0,2}%s\\s*=\\s*" + TEST_VALUES;
     @NotNull
     @Language( "RegExp" )
-    protected String castRegExp = "(?isu)(%s\\s*=.*?)\\bcast\\s*\\(\\s*"+ TEST_VALUES +"\\s*as\\s*("+ SQL_TYPE + ")\\s*\\)";
+    protected String castRegExp =
+            "(?isu)(%s\\s*=.*?)\\bcast\\s*\\(\\s*" + TEST_VALUES + "\\s*as\\s*(" + SQL_TYPE + ")\\s*\\)";
     @NotNull
-    @Language("RegExp")
+    @Language( "RegExp" )
     protected String columnName = "\\b([@#\\w&&[\\D]][\\w\\$@#]*)\\b";
     @NotNull
-    @Language("RegExp")
+    @Language( "RegExp" )
     protected String quotedColumnName = "\"(.+?)\"";
     @NotNull
-    @Language("RegExp")
+    @Language( "RegExp" )
     protected String columnNameInBrackets = "\\[(.+?)\\]";
     @NotNull
     @Language( "RegExp" )
-    protected String testValuesInInsert = "(?i)"+ TEST_VALUES + "\\s*[,\\)]";
+    protected String testValuesInInsert = "(?i)" + TEST_VALUES + "\\s*[,\\)]";
     @NotNull
-    @Language("RegExp")
+    @Language( "RegExp" )
     protected String insertRE =
             "(?isu)insert\\b\\s*\\binto\\b\\s*.+?\\s*(\\(\\s*(.+?)\\s*\\))?\\s*\\bvalues\\b\\s*\\((.+?\\))";
     @NotNull
--- src/test/java/com/kreig133/daogenerator/db/preparators/ValueModeQueryPreparatorTest.java
+++ src/test/java/com/kreig133/daogenerator/db/preparators/ValueModeQueryPreparatorTest.java
@@ -23,10 +23,10 @@ public class ValueModeQueryPreparatorTest extends ValueModeQueryPreparator {
 
     @Test
     public void columnsFromQueryTest() {
-        Map<String, String> columnsFromQuery = super.getColumnsFromQuery( QueryPreparatorTest.updateQuery );
+        Map<String, String> columnsFromQuery = super.getColumnsFromQuery( "UPDATE teSponsor SET sName = 'лжщл' WHERE nRecID = 97" );
         Assert.assertEquals( columnsFromQuery.size(), 2 );
-        Assert.assertTrue( columnsFromQuery.keySet().contains( "nflreport" ) );
-        Assert.assertTrue( columnsFromQuery.keySet().contains( "n_DEPO_id" ) );
+        Assert.assertTrue( columnsFromQuery.keySet().contains( "sName" ) );
+        Assert.assertTrue( columnsFromQuery.keySet().contains( "nRecID" ) );
     }
 
     @Test
