44aa9f1f20f74a914ecfd43c27c270d8d580cece
iziyangirov <iziyangirov@aplana.com>
2012-12-10 10:48:34 +0600

    Добавил генерацию кода для методов hashCode() и equals(). Написал к ним тесты.

--- src/main/java/com/kreig133/daogenerator/files/mybatis/InOutClassGenerator.java
+++ src/main/java/com/kreig133/daogenerator/files/mybatis/InOutClassGenerator.java
@@ -5,6 +5,8 @@ import com.google.common.collect.Iterators;
 import com.kreig133.daogenerator.enums.ClassType;
 import com.kreig133.daogenerator.enums.Scope;
 import com.kreig133.daogenerator.files.JavaClassGenerator;
+import com.kreig133.daogenerator.files.equalshashbuilder.EqualsBuilder;
+import com.kreig133.daogenerator.files.equalshashbuilder.HashBuilder;
 import com.kreig133.daogenerator.jaxb.NamingUtils;
 import com.kreig133.daogenerator.files.PackageAndFileUtils;
 import com.kreig133.daogenerator.jaxb.*;
@@ -88,7 +90,22 @@ public class InOutClassGenerator extends JavaClassGenerator {
             generateSetter( p );
         }
 
+        ParametersType params = new ParametersType();
+        for ( ParameterType p : parameters ) {
+            params.getParameter().add(p);
+        }
+        generateHashCodeEqualsMethod(params, this.name);
+
         writeToString();
+    }
+
+    private void generateHashCodeEqualsMethod(ParametersType params, String className){
+        insertTabs().append( EqualsBuilder.equalsBuilder(params, className) );
+        insertLine();
+        insertLine();
+        insertTabs().append( HashBuilder.hashBuilder(params) );
+        insertLine();
+        insertLine();
     }
 
     @Override
